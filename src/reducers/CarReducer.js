ff862b39b40bbd3876ea7d7ed673e73e58577d57
Bryant Patton <bryantpatton@gmail.com>
2019-08-15 11:17:27 -0700

    price total update

--- src/reducers/CarReducer.js
+++ src/reducers/CarReducer.js
@@ -4,25 +4,49 @@ import { initialState } from "./initialState";
 function CarReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_FEATURE:
-      return {
-        ...state,
-        car: {
-          ...state.car,
-          features: [...state.car.features, action.payload]
-        }
-      };
+      const featuresArrayIds = state.car.features.map(f => f.id);
+      if (featuresArrayIds.includes(action.payload.id)) {
+        return state;
+      } else {
+        const featuresArray = [...state.car.features, action.payload];
+        const priceTotal = featuresArray
+          .map(f => f.price)
+          .reduce((total, f) => {
+            return total + f;
+          });
+        return {
+          ...state,
+          car: {
+            ...state.car,
+            features: featuresArray
+          },
+          additionalPrice: priceTotal
+        };
+      }
+
     case REMOVE_FEATURES:
       const id = action.payload.id;
-      console.log(id);
       const newFeatures = state.car.features.filter(
         feature => feature.id !== id
       );
+      let priceTotal;
+      if (newFeatures.length > 0) {
+        priceTotal = newFeatures
+          .map(f => f.price)
+          .reduce((total, f) => {
+            return total + f;
+          });
+      } else {
+        priceTotal = 0;
+      }
+
       return {
         ...state,
         car: {
           ...state.car,
           features: newFeatures
-        }
+        },
+        additionalPrice: priceTotal
       };
     default:
       return state;
